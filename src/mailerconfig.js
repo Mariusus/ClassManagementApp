b28da0a48d4cb73517377ef1426b220321bcc55b
Mariusus <padazienius@gmail.com>
2023-01-25 01:41:37 +0100

    Mailer API fix

--- src/mailerconfig.js
+++ src/mailerconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-MAILGUN_USER: 'postmaster@sandbox72e46801126a487582ae187d955f910d.mailgun.org',
-MAILGUN_PASS: '1dabe440c50e2abbca9c733134bc347b-c9746cf8-e937b3b7'
+MAILGUN_USER: 'postmaster@sandboxd252ee38e7eb4df0a06571f41b719457.mailgun.org',
+MAILGUN_PASS: '5eedf3781fbc72085d1ec72443f6e610-c9746cf8-7de17515'
 };
